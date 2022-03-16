f1cecedbe09a06073f27fa99f63e404de04481fd
anna ch <cheshkoann@gmail.com>
2022-03-16 16:10:47 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -97,24 +97,6 @@ int main() {
     search_server.AddDocument(6, "cat funny and nasty rat"s, DocumentStatus::ACTUAL, { 1, 2 });
     search_server.GetWordFrequencies(8);
     RemoveDuplicates(search_server);
-    //int n = 25;
-    //string s = { "                         " };
-    //for (size_t i = 0; i < 10; i++) {
-    //    gen_random(s, n);
-    //    search_server.AddDocument(i + 6, s, DocumentStatus::ACTUAL, { 1 });
-    //}
-    //search_server.AddDocument(6, "cat", DocumentStatus::ACTUAL, { 1, 2 });
-    search_server.FindTopDocuments("fluffy well-groomed cat uncle Styopa"s, [](int document_id, DocumentStatus status, int rating) { return rating > 2; });
-    search_server.FindTopDocuments("", DocumentStatus::ACTUAL);
-    FindTopDocuments(search_server, "-cat funny -pet nasty rat");
-  /* DocumentStatus status = DocumentStatus::ACTUAL;
-   search_server.FindTopDocuments("", DocumentStatus::ACTUAL);*/
-   /*MatchDocuments(search_server, "cat nasty -hair");
-    map<string_view, double> a = search_server.GetWordFrequencies(3);
-    for (auto& [words, freq] : a) {
-        cout << words << " " << freq << endl;
-    }*/
-   // const string query = "curly and funny -not"s;
     TestSearchServer();
     return 0;
 }
--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Anna/source/repos/Final_project_plus_Test
+# Install script for directory: C:/Users/Анна/source/repos/SearchServer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Anna/source/repos/Final_project_plus_Test/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Анна/Source/Repos/SearchServer/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Anna/source/repos/Final_project_plus_Test/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Анна/source/repos/SearchServer/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
